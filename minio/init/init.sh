b8e1f0a653d2e732f4573ba37c8ada4f9cb7d037
Daniel J. B. Clarke <u8sand@gmail.com>
2019-06-19 16:41:22 -0400

    Automatically load all data with data-db-cli

--- minio/init/init.sh
+++ minio/init/init.sh
@@ -5,5 +5,13 @@ if [ ! -f initialized ]; then
   touch initialized
   mc mb minio/${S3_BUCKET}
 else
-  echo "s3 is already initialized.. quitting."
+  echo "s3 is already initialized.. triggering data load(s)"
+  for f in $(mc ls minio/${S3_BUCKET} | awk '{ print $5 }'); do
+    if 
+    curl \
+      -H 'Content-Type: application/json' \
+      -H "Authorization: Token ${TOKEN}" \
+      -X POST 'http://data-api/enrichmentapi/api/v1/load' \
+      -d "{\"bucket\": \"${BUCKET}\", \"file\": \"${f}\", \"datasetname\": \"$(basename ${f} .so)}\"}"
+  done
 fi
