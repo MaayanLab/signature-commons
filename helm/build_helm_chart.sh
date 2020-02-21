0ed76a1055cff57aac7f076604737a70d4530138
Daniel J. B. Clarke <u8sand@gmail.com>
2020-02-21 12:12:49 -0500

    Improved build_helm_chart when dealing with escaped variable substitutions

--- helm/build_helm_chart.sh
+++ helm/build_helm_chart.sh
@@ -12,24 +12,39 @@ helm template --debug \
   -f ../docker-compose.data-db.yml \
   > "${output}/${output}.yaml.stage0"
 
-# Replace variables with .Values
-sed \
-  -e 's/\${\([^}]\+\)}/{{ .Values.\1 }}/g' \
-  -e "s/helm-chart-docker-compose/${output}/g" \
-  > "${output}/${output}.yaml.stage1" \
-  < "${output}/${output}.yaml.stage0"
-
 # Generate values.yaml with .env.example
 sed \
   -e 's/^\([^=]\+\)=\(.*\)$/\1: \2/g' \
-  > "${output}/Values.yaml" \
-  < ../.env.example
+  < ../.env.example \
+  > "${output}/Values.yaml"
+
+# Generate .Values substitution with .env.example
+sed \
+  -e 's/^\([^=]\+\)=\(.*\)$/export \1="{{ .Values.\1 }}"/g' \
+  < ../.env.example \
+  > "${output}/.env"
+
+# Substitute variables with envsubst
+. ${output}/.env
+export _DOLLAR='$'
+sed -e 's/\$\$/${_DOLLAR}/g' \
+  < "${output}/${output}.yaml.stage0" \
+  > "${output}/${output}.yaml.stage1"
+envsubst \
+  < "${output}/${output}.yaml.stage1" \
+  > "${output}/${output}.yaml.stage2"
+
+# Replace helm-chart-docker-compose
+sed \
+  -e "s/helm-chart-docker-compose/${output}/g" \
+  < "${output}/${output}.yaml.stage2" \
+  > "${output}/${output}.yaml.stage3"
 
 # Split file into separate files
 mkdir -p "${output}/templates"
 python3 -c "
 import os, re
-for content in map(str.strip, open(os.path.join('${output}', '${output}.yaml.stage1'), 'r').read().split('---')):
+for content in map(str.strip, open(os.path.join('${output}', '${output}.yaml.stage3'), 'r').read().split('---')):
   m = re.search('# Source: (.+)\n', content)
   if m:
     filename = m.group(1)
@@ -40,4 +55,7 @@ for content in map(str.strip, open(os.path.join('${output}', '${output}.yaml.sta
 echo "Cleaning up..."
 rm \
   "${output}/${output}.yaml.stage0" \
-  "${output}/${output}.yaml.stage1"
+  "${output}/${output}.yaml.stage1" \
+  "${output}/${output}.yaml.stage2" \
+  "${output}/${output}.yaml.stage3" \
+  "${output}/.env"
